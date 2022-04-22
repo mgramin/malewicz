27e281d375112f0ebf5ebe2696ee559e20fd2367
mgramin <mgramin@querifylabs.com>
2022-04-23 00:22:54 +0300

    fix table size

--- templates/sql/big_tables.sql
+++ templates/sql/big_tables.sql
@@ -5,7 +5,7 @@ select table_name
              , rank() over (order by total_size desc) as rank
              , total_size
           from (select table_name
-                     , pg_total_relation_size(quote_ident(table_name)) as total_size
+                     , pg_total_relation_size(table_schema || '.' || table_name) as total_size
                   from information_schema.tables
                  where table_schema not in ('pg_catalog', 'information_schema')
                    and table_type = 'BASE TABLE'
